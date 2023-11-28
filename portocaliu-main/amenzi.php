e3404c6fd6dfbc43bcfb075716ad147e2cc1278e
AdrianNichiforeac <145973841+AdrianNichiforeac@users.noreply.github.com>
2023-11-28 10:55:20 +0200

    Update amenzi.php

--- portocaliu-main/amenzi.php
+++ portocaliu-main/amenzi.php
@@ -6,7 +6,7 @@
     $config = require_once "config.php";
     $db = new DataBase($config);
 
-    $query = $db->query('Select * from amenzi', [], PDO::FETCH_OBJ);
+    $query = $db->query('Select * from amenzi ORDER BY nume', [], PDO::FETCH_OBJ);
     if(isset($_POST['nume']) && isset($_POST['faptas'])){
         $insert = $db->query("Insert into amenzi(nume, faptas) values(:nume, :faptas)", [
             'nume'      => $_POST['nume'],
